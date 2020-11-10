6c0e3060bd2e737fa468f472324561c9c276a108
Marcelino Gonzalez <agemarcelino@hotmail.com>
2020-11-09 21:39:06 -0600

    Create redirect on main

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,8 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   if (!req.cookies['user']) {
-    res.redirect('/account/login')
   } else {
-    res.render('index', { title: 'Express' });
+    res.redirect('/logs')
   }
 });
 
